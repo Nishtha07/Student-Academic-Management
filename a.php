7763afbdccbb24936bcd35501097969fb4750647
Nishtha07 <pnishtha15@gmail.com>
2019-04-01 11:28:30 +0530

    first

---
+++ a.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>..</title>
+<style>
+a:link{COLOR: #666666; TEXT-DECORATION: none}
+a:visited{COLOR: #666666; TEXT-DECORATION: none}
+a:active{COLOR: #666666; TEXT-DECORATION: none}
+a:hover{COLOR: #666666; TEXT-DECORATION: underline}
+.s1{font-family: 'Courier New'; font-size:12px}
+</style>
+</head>
+<body topmargin="0" leftmargin="0" style="font-family: Verdana; font-size: 8pt; background-image: url('watermark.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-position: center">
+<table align='center' border='0' cellpadding='0' cellspacing='0' style='border-collapse: collapse' width='780' height=40%>
+<tr><td height=98% align=center> 
+<B><u>UPDATE MARKS</u></B><BR><BR>
+<table border="2" cellpadding="5" cellspacing="10" width="70%" style="border-collapse: collapse; font-size: 11px">
+<FORM METHOD="get" name="f1">
+    <tr>
+    <td> ROLL NO. : </td><td><input type="text" name="rollno" maxlength="12" size="20" style="font-size: 10px"></td>
+    <td> TOTAL MARKS : </td><td><input type="text" name="marks" maxlength="12" size="20" style="font-size: 10px"></td>
+    <td><input type="button" onClick="document.f1.submit()" size="20" value="Submit" style="font-size: 10px">
+    </td></tr>
+    </FORM>
+    </table>
+    </BR></BR></td></tr></table>
+    
+    <table align='center' border='0' cellpadding='0' cellspacing='0' style='border-collapse: collapse' width='780' >
+<tr><td height=98% align=center> 
+<B><u>UPDATE ATTENDANCE</u></B><BR><BR>
+<table border="2" cellpadding="5" cellspacing="10" width="70%" style="border-collapse: collapse; font-size: 11px">
+<FORM METHOD="get" name="f1">
+    <tr>
+    <td> ROLL NO. : </td><td><input type="text" name="rollno" maxlength="12" size="20" style="font-size: 10px"></td>
+    <td> TOTAL ATTENDANCE : </td><td><input type="text" name="marks" maxlength="12" size="20" style="font-size: 10px"></td>
+    <td><input type="button" onClick="document.f1.submit()" size="20" value="Submit" style="font-size: 10px">
+    </td></tr>
+    </FORM>
+    </table>
+    </BR></BR></td></tr></table>
+    
+    </body></html>
